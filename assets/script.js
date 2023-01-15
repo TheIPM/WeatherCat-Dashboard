7905779a1ce9b6a3ebb0296cc388396f0b4d5f8b
TheIPM <islamluse@gmail.com>
2023-01-15 23:57:10 +1030

    added more js, added the 5 days in advanced with hours 3hr intervals

--- assets/script.js
+++ assets/script.js
@@ -18,12 +18,26 @@ function getWeather(location) {
         return response.json();
       })
       .then(function(data) {
-        console.log(data);
-        var temperature = data.list[0].main.temp; // temperature in kelvin
-        var temperatureInCelsius = temperature - 273.15;
+        var forecastData = data.list;
+        for(var i = 0; i < forecastData.length; i++) {
+            var forecast = forecastData[i];
+            var date = new Date(forecast.dt*1000);
+            var temperature = forecast.main.temp - 273.15;
+            var weatherDescription = forecast.weather[0].description;
+            var weatherIcon = forecast.weather[0].icon;
+            // Create new HTML element to display the forecast
+            var forecastContainer = document.createElement('div');
+            forecastContainer.innerHTML = `
+                <div class="forecast-date">${date}</div>
+                <img src="http://openweathermap.org/img/w/${weatherIcon}.png" alt="Weather Icon">
+                <div class="forecast-temp">${temperature}C</div>
+                <div class="forecast-desc">${weatherDescription}</div>
+            `;
+            // Append the new element to the forecast container
+            document.getElementById('forecast-container').appendChild(forecastContainer);
+        }
+        var temperature = data.list[0].main.temp - 273.15; // temperature in kelvin
         var temperatureElement = document.getElementById('temperature');
-        temperatureElement.innerHTML = temperatureInCelsius + 'C';
+        temperatureElement.innerHTML = temperature + 'C';
       });
-      
   }
-
