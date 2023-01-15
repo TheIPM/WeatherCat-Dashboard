5d75a889ea00ebe5b1eefcee47c749255ba5c98a
TheIPM <islamluse@gmail.com>
2023-01-16 00:01:31 +1030

    made it so its just 5 days

--- assets/script.js
+++ assets/script.js
@@ -14,30 +14,35 @@ function getWeather(location) {
     var url = `http://api.openweathermap.org/data/2.5/forecast?q=${location}&appid=${apiKey}`;
   
     fetch(url)
-      .then(function(response) {
-        return response.json();
-      })
-      .then(function(data) {
-        var forecastData = data.list;
-        for(var i = 0; i < forecastData.length; i++) {
-            var forecast = forecastData[i];
-            var date = new Date(forecast.dt*1000);
-            var temperature = forecast.main.temp - 273.15;
-            var weatherDescription = forecast.weather[0].description;
-            var weatherIcon = forecast.weather[0].icon;
-            // Create new HTML element to display the forecast
-            var forecastContainer = document.createElement('div');
-            forecastContainer.innerHTML = `
-                <div class="forecast-date">${date}</div>
-                <img src="http://openweathermap.org/img/w/${weatherIcon}.png" alt="Weather Icon">
-                <div class="forecast-temp">${temperature}C</div>
-                <div class="forecast-desc">${weatherDescription}</div>
-            `;
-            // Append the new element to the forecast container
-            document.getElementById('forecast-container').appendChild(forecastContainer);
-        }
-        var temperature = data.list[0].main.temp - 273.15; // temperature in kelvin
-        var temperatureElement = document.getElementById('temperature');
-        temperatureElement.innerHTML = temperature + 'C';
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      var forecastData = data.list;
+      var filteredData = forecastData.filter(function(forecast) {
+        var date = new Date(forecast.dt * 1000);
+        var hours = date.getUTCHours();
+        return hours === 12;
       });
+      for(var i = 0; i < filteredData.length; i++) {
+          var forecast = filteredData[i];
+          var date = new Date(forecast.dt*1000);
+          var temperature = forecast.main.temp - 273.15;
+          var weatherDescription = forecast.weather[0].description;
+          var weatherIcon = forecast.weather[0].icon;
+          // Create new HTML element to display the forecast
+          var forecastContainer = document.createElement('div');
+          forecastContainer.innerHTML = `
+              <div class="forecast-date">${date}</div>
+              <img src="http://openweathermap.org/img/w/${weatherIcon}.png" alt="Weather Icon">
+              <div class="forecast-temp">${temperature}C</div>
+              <div class="forecast-desc">${weatherDescription}</div>
+          `;
+          // Append the new element to the forecast container
+          document.getElementById('forecast-container').appendChild(forecastContainer);
+      }
+      var temperature = data.list[0].main.temp - 273.15; // temperature in kelvin
+      var temperatureElement = document.getElementById('temperature');
+      temperatureElement.innerHTML = temperature + 'C';
+    });
   }
