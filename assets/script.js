c1783339ab13a38b43fb0aaa4e967a7e54efbd87
TheIPM <islamluse@gmail.com>
2023-01-16 11:13:41 +1030

    added humidity and windspeed

--- assets/script.js
+++ assets/script.js
@@ -10,39 +10,44 @@ weatherForm.addEventListener('submit', function(event) {
 });
 
 function getWeather(location) {
-    var apiKey = '16962282faecdfd494ff4b8f615e26b8';
-    var url = `http://api.openweathermap.org/data/2.5/forecast?q=${location}&appid=${apiKey}`;
-    document.getElementById('forecast-container').innerHTML = '';
-    fetch(url)
+  var apiKey = '16962282faecdfd494ff4b8f615e26b8';
+  var url = `http://api.openweathermap.org/data/2.5/forecast?q=${location}&appid=${apiKey}`;
+
+  fetch(url)
     .then(function(response) {
       return response.json();
     })
     .then(function(data) {
-      var forecastData = data.list
-      var filteredData = forecastData.filter(function(forecast) {
-        var date = new Date(forecast.dt * 1000);
-        var hours = date.getUTCHours();
-        return hours === 12;
-      });
-      for(var i = 0; i < filteredData.length; i++) {
-          var forecast = filteredData[i];
+      var forecastData = data.list.slice(0,40);
+      var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+      var currentDay = '';
+      var forecastContainer = document.getElementById('forecast-container');
+      forecastContainer.innerHTML = '';
+      for(var i = 0; i < forecastData.length; i++) {
+          var forecast = forecastData[i];
           var date = new Date(forecast.dt*1000);
-          var temperature = forecast.main.temp - 273.15;
-          var weatherDescription = forecast.weather[0].description;
-          var weatherIcon = forecast.weather[0].icon;
-          // Create new HTML element to display the forecast
-          var forecastContainer = document.createElement('div');
-          forecastContainer.innerHTML = `
-              <div class="forecast-date">${date}</div>
-              <img src="http://openweathermap.org/img/w/${weatherIcon}.png" alt="Weather Icon">
-              <div class="forecast-temp">${temperature}C</div>
-              <div class="forecast-desc">${weatherDescription}</div>
-          `;
-          // Append the new element to the forecast container
-          document.getElementById('forecast-container').appendChild(forecastContainer);
+          var day = days[date.getUTCDay()];
+          var hours = date.getUTCHours();
+          var windSpeed = forecast.wind.speed;
+          var humidity = forecast.main.humidity;
+          if (currentDay !== day && hours === 12) {
+              currentDay = day;
+              var temperature = forecast.main.temp - 273.15;
+              var weatherDescription = forecast.weather[0].description;
+              var weatherIcon = forecast.weather[0].icon;
+              // Create new HTML element to display the forecast
+              var forecastContainer = document.createElement('div');
+              forecastContainer.innerHTML = `
+                  <div class="forecast-date">${day}</div>
+                  <img src="http://openweathermap.org/img/w/${weatherIcon}.png" alt="Weather Icon">
+                  <div class="forecast-temp">${temperature}C</div>
+                  <div class="forecast-wind-speed">Wind Speed: ${windSpeed}</div>
+                  <div class="forecast-humidity">Humidity: ${humidity}%</div>
+                  <div class="forecast-desc">${weatherDescription}</div>
+              `;
+              // Append the new element to the forecast container
+              document.getElementById('forecast-container').appendChild(forecastContainer);
+          }
       }
-      var temperature = data.list[0].main.temp - 273.15; // temperature in kelvin
-      var temperatureElement = document.getElementById('temperature');
-      temperatureElement.innerHTML = temperature + 'C';
     });
-  }
+}
